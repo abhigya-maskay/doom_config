30c16304a05e8b9d27aa2d2c7df4d4913808f9bb
Abhigya Maskay <ave70011@gmail.com>
2025-06-04 23:46:52 -0400

    Added catppuccin theme

--- packages.el
+++ packages.el
@@ -13,6 +13,8 @@
 (unpin! org-roam)
 (package! org-roam-ui)
 
+(package! catppuccin-theme)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
