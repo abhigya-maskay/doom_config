011686918685d7d16ce3af1ad79831b72657fd13
Abhigya Maskay <abhigya.maskay@bluesight.com>
2025-06-09 10:09:06 -0400

    Added some python setup

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" default)))
+   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" default))
+ '(safe-local-variable-values
+   '((lsp-pyright-python-executable-cmd . "/Users/AbhigyaMaskay/.local/share/virtualenvs/universal-rfid-reader-interface-24KQmMil/bin/python"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
